3a3d99b2af6539b407563cfc79e13586c84832f9
zihaoz12 <zzhaa12@gmail.com>
2019-06-08 11:35:18 -0700

    install cores

--- server.js
+++ server.js
@@ -11,10 +11,19 @@ const AdminController = require('./controllers/adminController');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, // This allows the session cookie to be sent back and forth
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
+app.use(cors(corsOptions));
+  
 app.get('/',(req,res)=>{
     res.render('index.ejs')
 })
+
+
 
 app.listen(PORT,()=>{
 	console.log("App is listening at : ", PORT);
