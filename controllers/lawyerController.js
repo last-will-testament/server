4877ab0bfbc7c3d695e9080f89398059510b5b9b
zihaoz12 <zzhaa12@gmail.com>
2019-06-08 16:51:46 -0700

    work in progress

--- controllers/lawyerController.js
+++ controllers/lawyerController.js
@@ -2,19 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Lawyer = require('../models/Lawyer');
 
-// router.post('/', async(req, res) => {
-//     try{
-//       const createdLawyerr= await Users.create(req.body);
-//       console.log(`Created Users: ${createdUser}`);
-//       res.json({
-//         status:200,
-//         data: createdUser
-//       })
-//       console.log("post success")
-//     }catch(err){
-//       res.send(err)
-//     }
-// });
+
 
 //GET all wills..
 router.get('/', async(req, res) => {
@@ -33,10 +21,6 @@ router.get('/', async(req, res) => {
 router.post('/', async(req, response) => {
   try{
     const createdLawyer = await Lawyer.create(req.body);
-    //we don't have login so we don't need this....
-    // createdWills.authorId = req.session.userId;
-    // createdWills.authorname = req.session.username;
-
     createdLawyer.save((err, savedLawyer) => {
       response.json({
         status: 200,
